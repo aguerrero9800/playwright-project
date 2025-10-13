8fca8bf2e5603e12ec073d58c97b3cc6a07d4660
Adrian Guerrero <adrianjguerreros@gmail.com>
2025-10-13 16:19:47 -0300

    propiedad web scraping

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { title } from 'process';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -24,6 +25,14 @@ test('test', async ({ page }) => {
   await page.keyboard.press('Enter')
 
   await expect(page.locator('//ol[contains( @class, \'ui-search-layout\')]')).toBeVisible()
-  await page.pause()
+  //await page.pause()
+
+  const titles = await page.locator('//ol[contains(@class,"ui-search-layout")]//h3[contains(@class,"poly-component__title-wrapper")]').allInnerTexts();
+
+  console.log('the total number of result is:', titles.length)
+
+  for (let title of titles) {
+    console.log(' the title is:', title)
+  }
 
 });
