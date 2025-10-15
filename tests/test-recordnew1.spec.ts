8aaf5d9dfab79b349c54d354af9a829dc802c0ac
Adrian Guerrero <adrianjguerreros@gmail.com>
2025-10-15 20:39:42 -0300

    Feature: Recordnew1

---
+++ tests/test-recordnew1.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://mercadolibre.com/');
+  await page.getByRole('link', { name: 'Argentina' }).click();
+  await page.getByRole('combobox', { name: 'Ingresá lo que quieras' }).click();
+  await page.getByRole('combobox', { name: 'Ingresá lo que quieras' }).fill('iphone');
+  await page.getByRole('combobox', { name: 'Ingresá lo que quieras' }).click();
+  await page.getByRole('button', { name: 'Buscar' }).click();
+  await page.getByRole('link', { name: 'Apple iPhone 13 (128 GB) - Medianoche - Distribuidor Autorizado' }).click();
+  await page.getByRole('button', { name: 'Comprar ahora' }).first().click();
+
+  await await page.pause()
+  
+});
